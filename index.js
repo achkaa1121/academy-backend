115019f4c93c20bb2b0c456578138d7876daec06
Backend12 <backend12@Erxess-Mac-mini.local>
2025-10-23 20:27:12 +0800

    Finally

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const mail = document.getElementById("username");
 const pass = document.getElementById("password");
-
-
 const but = document.getElementById("loginBtn");
 const user = {
     Email: "admin@erxes.io",
     Password: 1234
 };
-// but.innerText = "Login";
-
+but.innerText = "Login";
 const check = () => {
-    console.log(mail,'mail')
-        console.log(pass,'pass')
-    if(mail !== user.Email || pass !== user.Password){
+    const userName = mail.value;
+    const passWord = pass.value;
+    if(userName != user.Email || passWord != user.Password){
     window.alert("Your password or email is wrong")
 }else{
     window.alert("Logged in succesfully")
