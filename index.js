23111f15a32f75c5efd61fdce081776471ccf749
Backend12 <backend12@Erxess-Mac-mini.local>
2025-10-27 20:42:11 +0800

    Not yet

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-let num = 0;
-const counter = document.getElementById("counter");
-const nemehBut = document.getElementById("buttonNemeh");
-const hasahBut = document.getElementById("buttonHasah");
-nemehBut.innerText = "Nemeh";
-hasahBut.innerText = "Hasah";
-const plus = () => {
-        num++;
-    counter.innerText = num;
+import { dice } from "./games/dice.js";
+import { rockPaper } from "./games/rockpaperscissor.js";
+import { guessNum } from "./games/guessthenumber.js";
+import { guessWord } from "./games/guesstheword.js";
+const dicegame = document.getElementById("dice");
+const leave = document.getElementById("leave");
+const rockpaper = document.getElementById("rockPaper");
+const num = document.getElementById("guessNum");
+const word = document.getElementById("guessWord");
+const resulArray = [];
+const isClicked = () => {
 }
-const minus = () => {
-        num--;
-    counter.innerText = num;
+while(true){
+    switch(){
+        case 2:
+            const gameResult = dice()
+            resulArray.push(gameResult)
+            break;
+        case 3:
+            guessWord()
+            break;
+        case 4:
+            guessNum()
+            break;
+        case 1 :
+            rockPaper()
+            break;
+    }
+    let wth = Number(window.prompt("Keep playing = 1, Stop playing 0, Play other games = 2"));
+    if(wth === 0){
+        break;
+    }else if(wth === 2){
+        currentGame = Number(window.prompt("Ymar togloom togloh ve? RockPaper bol 1, dice bol 2, ug taah bol 3 , toog bol 4"));
+    }
 }
-nemehBut.addEventListener("click",plus);
-hasahBut.addEventListener("click",minus);
