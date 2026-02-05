a4b187c61da4d8f054ad5930d64fa592433cc288
Backend12 <backend12@Erxess-Mac-mini.local>
2026-02-05 20:24:33 +0800

    update

--- src/movies/router.ts
+++ src/movies/router.ts
@@ -4,8 +4,17 @@ import { addMovie } from "./controller.ts";
 export const movieRouter = Router();
 
 movieRouter.get("/movies", async (req: Request, res: Response) => {
-  console.log("movies");
-  const movies = await Movies.find({}).limit(100);
+  const { genre } = req.query;
+
+  const query = {} as any;
+
+  if (genre) {
+    query.genres = genre;
+  }
+
+  const movies = await Movies.find(query).limit(100);
+
+  res.json(movies);
 });
 movieRouter.post("/addMovie", async (req: Request, res: Response) => {
   const { title } = req.body;
