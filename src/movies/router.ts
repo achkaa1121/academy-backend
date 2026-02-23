a959da8b44d48f65119bb771054f1716043e2382
Backend12 <backend12@Erxess-Mac-mini.local>
2026-02-23 20:11:32 +0800

    update

--- src/movies/router.ts
+++ src/movies/router.ts
@@ -3,8 +3,12 @@ import { Movies } from "./models.ts";
 export const movieRouter = Router();
 
 movieRouter.get("/movies", async (req: Request, res: Response) => {
-  const movies = await Movies.find().limit(100);
-  res.json(movies);
+  const pageNumber = Number(req.query.page) || 1;
+  const skip = (pageNumber - 1) * 20;
+  const movies = await Movies.find().skip(skip).limit(20);
+  const totalMovies = await Movies.countDocuments();
+  const totalPages = Math.ceil(totalMovies / 20);
+  res.json({ movies, totalPages });
 });
 movieRouter.get("/topMovies", async (req, res) => {
   const topMovies = await Movies.find().sort({ "imdb.rating": -1 }).limit(100);
